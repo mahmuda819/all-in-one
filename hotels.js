c24497a3aae1048b50d88d43b67e56600f8bed0a
Tania <mahmuda77819@gmail.com>
2026-08-31 19:46:39 +0600

    Fix image file extension for Allahor Dan Biryani House

--- hotels.js
+++ hotels.js
@@ -15,7 +15,7 @@ const hotelsData = [
   {
     id: "hotel-001",
     name: "আল্লাহর দান বিরিয়ানি হাউজ",
-    image: "restu/allahordan.jpeg",
+    image: "restu/allahordan.jpg",
     category: "Hotel's Food",
     location: "College Gate",
     rating: 4.4,
