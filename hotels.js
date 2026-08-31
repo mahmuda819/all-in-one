1741e9a5830638c2f9cbc944023d0186229be928
Tania <mahmuda77819@gmail.com>
2026-08-31 19:23:26 +0600

    Update hotel image filename in hotelsData

--- hotels.js
+++ hotels.js
@@ -15,7 +15,7 @@ const hotelsData = [
   {
     id: "hotel-001",
     name: "আল্লাহর দান বিরিয়ানি হাউজ",
-    image: "assets/hotels/college-gate-grand.svg",
+    image: "allahordan.jpeg",
     category: "Hotel's Food",
     location: "College Gate",
     rating: 4.4,
